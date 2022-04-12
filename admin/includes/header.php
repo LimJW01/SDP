fd282f172baf515ae9d9230c9bd3cc7bf9658aa8
Ellyis <kaibinyong2002@gmail.com>
2022-04-12 10:10:33 +0800

    title

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -16,7 +16,7 @@ session_start();
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>MediLife</title>
+    <title>ClubExpress</title>
     <?php
     include_once "includes/links.php";
     include_once "includes/scripts.php";
