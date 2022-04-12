4698a5d110b799e58fe24798841bc8233bbfc0c1
JoeLJW <jooweelim@gmail.com>
2022-04-12 22:47:34 +0800

    added some pages for members and comittee

---
+++ tbc/event.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="shortcut icon" type="image/x-icon"
+        href="https://i.pinimg.com/originals/ef/f3/bf/eff3bf95d0e3c87864867cdecc4e445d.png" />
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Source+Code+Pro:wght@300&display=swap"
+        rel="stylesheet">
+
+    <style>
+        .title {
+        margin-left: 200px;
+        }
+
+        figure {
+        margin-left: 250px;
+        }
+
+        figcaption {
+        text-align: center;
+        }
+    </style>
+    <title>Events</title>
+
+</head>
+<body>
+    <?php include('navbar.php');?>  
+    <div class="title">
+        <h1>Events<br></h1>
+    </div>
+    <div class="section1">
+        <a onclick="location.href='club.php';">
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+            <figure class="figure">
+                <img src="image/taekwondo.png" width="400" height="350">
+                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+            </figure>
+        </a>
+    </div>
+    <footer class="footer_section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 footer-col">
+                    <div class="col-md-4 footer-col">
+                        <div class="footer_detail">
+                            <a class="footer-logo">
+                                ClubExpress
+                            </a>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos debitis non corporis cupiditate ad totam pariatur accusamus vel, dolores maxime? Optio recusa
+                            </p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-4 footer-col">
+                    <h4>
+                            Quick Links
+                        </h4>
+                        <p>
+                            <a href="">Clubs</a>
+                            </p>
+                        <p>
+                            <a href="">Events</a>
+                        </p>
+                        <p>
+                            <a href="">About Us</a>
+                        </p>
+                        <p>
+                            <a href="">Contact Us   </a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 footer-col">
+                    <div class="footer_contact">
+                            <h4>
+                                Information
+                            </h4>
+                            <div class="contact_link_box">
+                                <a>
+                                    <i class="fa fa-phone" aria-hidden="true"></i>
+                                    <span>
+                                        Email: admin@gmail.com
+                                    </span>
+                                </a>
+                                <a>
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                    <span>
+                                        Phone: 012-3456789
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-info">
+                    <p>
+                        &copy; <span id="displayYear"></span> 2022 ClubExpress
+                        <a>All Rights Reserved</a><br><br>
+                    </p>
+                </div>
+            </div>
+        </footer>
+</body>
+</html>
