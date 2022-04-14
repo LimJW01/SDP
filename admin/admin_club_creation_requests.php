456279c28e38899149d05259d18af2962f5556e7
Ellyis <kaibinyong2002@gmail.com>
2022-04-14 12:42:50 +0800

    init

---
+++ admin/admin_club_creation_requests.php
@@ -0,0 +1,152 @@
+<?php
+include_once "includes/header.php";
+include_once "../change_time_format.php";
+?>
+<main id="main">
+    <h1 class="title">Club Creation Requests</h1>
+    <br>
+    <hr>
+    <article id="club-creation-requests">
+        <div class="content-container">
+            <button data-modal-target="#add" title="Add Request" id="add-button">Add Request</button>
+            <div class="table-container">
+                <table>
+                    <tr>
+                        <th class="padding-left">Club Image</th>
+                        <th class="padding-left">Club Name</th>
+                        <th class="padding-left">Club Description</th>
+                        <th class="padding-left">Student Name</th>
+                        <th class="padding-left">Student Contact Number</th>
+                        <th style="text-align: center;">Actions</th>
+                    </tr>
+                    <?php
+                    $club_creation_sql = "SELECT * FROM club_creation ORDER BY Club_creation_ID ASC";
+                    $club_creation_result = $conn->query($club_creation_sql);
+                    $club_creation_result_check = mysqli_num_rows($club_creation_result);
+                    $club_creation_array = array();
+                    ?>
+                    <?php if ($club_creation_result_check > 0) : ?>
+                    <?php while ($row = mysqli_fetch_assoc($club_creation_result)) : ?>
+                    <?php
+                            $club_creation_id = "C" . $row['Club_creation_ID'];
+                            array_push($club_creation_array, $club_creation_id);
+                            ?>
+                    <tr>
+                        <td style="text-align: center;"><img
+                                src="data:image/jpeg;base64,<?php echo base64_encode($row['Club_image']); ?>"
+                                alt="club_image">
+                        </td>
+                        <td class="padding-left"><?php echo $row['Club_name']; ?></td>
+                        <td class="padding-left"><?php echo $row['Club_description']; ?></td>
+                        <td class="padding-left"><?php echo $row['Student_name']; ?></td>
+                        <td class="padding-left"><?php echo $row['Student_contact_number']; ?></td>
+                        <td style="text-align: center;">
+                            <i data-modal-target="#view" title="View" class="fas fa-eye"
+                                id="view-button-<?php echo $club_creation_id; ?>"></i>
+                            <i data-modal-target="#edit" title="Edit" class="fas fa-edit"
+                                id="edit-button-<?php echo $club_creation_id; ?>"></i>
+                            <a href="admin_club_creation_requests.php"><i title="Delete" class="fas fa-trash-alt"
+                                    id="delete-button-<?php echo $club_creation_id; ?>"></i>
+                            </a>
+                        </td>
+                    </tr>
+                    <?php endwhile; ?>
+                    <?php else : ?>
+                    <tr>
+                        <td colspan="5">
+                            <h2 class="no-record">No Records Found</h2>
+                        </td>
+                    </tr>
+                    <?php endif; ?>
+                </table>
+            </div>
+        </div>
+    </article>
+
+    <!-- View Event -->
+    <div class="modal" id="view">
+        <!-- Modal content -->
+        <div class="modal-content" id="view-event">
+            <button close-button class="close">&times;</button>
+            <h1>View Event Details</h1>
+            <form id="view-form">
+            </form>
+        </div>
+    </div>
+
+    <!-- Edit Event -->
+    <div class="modal" id="edit">
+        <!-- Modal content -->
+        <div class="modal-content" id="edit-event">
+            <button close-button class="close">&times;</button>
+            <h1>Edit Event Details</h1>
+            <form action="manage_event.php" id="edit-form" method="post" enctype="multipart/form-data">
+            </form>
+        </div>
+    </div>
+
+    <!-- Add Event -->
+    <div class="modal" id="add">
+        <!-- Modal content -->
+        <div class="modal-content" id="add-event">
+            <button close-button class="close">&times;</button>
+            <h1>Add New Event</h1>
+            <form action="manage_event.php" id="add-form" method="post" enctype="multipart/form-data">
+            </form>
+        </div>
+    </div>
+    <div id="overlay"></div>
+</main>
+
+<script>
+$(document).ready(function() {
+    <?php foreach ($event_array as $event_id) : ?>
+
+    // Load View Event Data When Clicked
+    $("#view-button-<?php echo $event_id; ?>").click(function() {
+        var id = "<?php echo str_replace("E", "", $event_id) ?>";
+        var action = "view";
+        $("#view-form").load("manage_event_data.php", {
+            id: id,
+            action: action
+        });
+    });
+
+    // Load Edit Event Data When Clicked
+    $("#edit-button-<?php echo $event_id; ?>").click(function() {
+        var id = "<?php echo str_replace("E", "", $event_id) ?>";
+        var action = "edit";
+        $("#edit-form").load("manage_event_data.php", {
+            id: id,
+            action: action
+        });
+    });
+
+    // Load Delete Event Data When Clicked
+    $("#delete-button-<?php echo $event_id; ?>").click(function() {
+        if (confirm("Are you sure you want to delete this record?")) {
+            var id = "<?php echo str_replace("E", "", $event_id) ?>";
+            var action = "delete";
+            $(window).load("manage_event_data.php", {
+                id: id,
+                action: action
+            });
+        }
+    });
+
+    <?php endforeach; ?>
+
+    // Load Add Event Data When Clicked
+    $("#add-button").click(function() {
+        var action = "add";
+        $("#add-form").load("manage_event_data.php", {
+            action: action
+        });
+    });
+});
+</script>
+
+<?php
+include_once "includes/alert_message.php";
+include_once "includes/footer.php";
+?>
