2dc3aea9826e572df22c2884b92add05e0fb4ee6
Kohiro <liqi2002@gmail.com>
2022-04-18 19:18:20 +0800

    club

--- admin/admin_specific_club.php
+++ admin/admin_specific_club.php
@@ -7,25 +7,29 @@ $club_name = $_GET['club'];
 $club_sql = "SELECT * FROM clubs WHERE Name = '$club_name';";
 $club_details = $conn->query($club_sql);
 $club_row = mysqli_fetch_assoc($club_details);
-$_SESSION['club_id'] = $club_row['Club_ID'];
+$club_id = $club_row['Club_ID'];
+$_SESSION['club_id'] = $club_id;
 ?>
 
 <main id="main">
-    <h1 class="title"><?php echo $club_row['Name']?></h1>
+    <h1 class="title"><?php echo $club_row['Name'] ?></h1>
     <br>
     <hr>
     <article id="specific-club">
         <div class="content-container" id="specific-club-details">
             <div class="title-container">
                 <h2>Club Settings</h2>
-                <button title="Delete" id="delete-button">
-                    <i class="fas fa-trash-alt"></i>Delete
-                </button>
+                <form action="manage_specific_club.php" method="post">
+                    <button title="Delete" name="delete" type="submit" id="delete-button">
+                        <i class="fas fa-trash-alt"></i>Delete
+                    </button>
+                </form>
                 <button title="Edit" id="edit-button">
                     <i class="fas fa-edit"></i>Edit
                 </button>
             </div>
-            <form action="manage_specific_club.php" method="post" onsubmit="return validate_edit_club();">
+            <form action="manage_specific_club.php" method="post" enctype="multipart/form-data"
+                onsubmit="return validate_edit_club();">
                 <img id="club-image" src="data:image/jpeg;base64,<?php echo base64_encode($club_row['Image']); ?>"
                     alt='club_image'>
                 <ul class="flex-container">
@@ -128,7 +132,75 @@ $_SESSION['club_id'] = $club_row['Club_ID'];
             </form>
         </div>
     </article>
+
+    <article id="specific-club-member-list">
+        <div class="content-container">
+            <button data-modal-target="#add" title="Add Member" id="add-button">Add Member</button>
+            <div class="table-container">
+                <table>
+                    <tr>
+                        <th class="padding-left">Student Name</th>
+                        <th class="padding-left">TP Number</th>
+                        <th class="padding-left">Gender</th>
+                        <th class="padding-left">Contact Number</th>
+                        <th class="padding-left">Role</th>
+                        <th style="text-align: center;">Actions</th>
+                    </tr>
+                    <?php
+                    $student_sql = "SELECT * FROM students ORDER BY Student_name ASC";
+                    $student_result = $conn->query($student_sql);
+                    $student_result_check = mysqli_num_rows($student_result);
+                    $student_array = array();
+                    ?>
+                    <?php if ($student_result_check > 0) : ?>
+                    <?php while ($student_row = mysqli_fetch_assoc($student_result)) : ?>
+                    <?php
+                            $student_ID = "S" . $student_row['Student_ID'];
+                            array_push($student_array, $student_ID);
+
+                            $joined_club_sql = "SELECT * FROM joined_clubs WHERE Club_ID = '$club_id' AND Student_ID = " . $student_row['Student_ID'] .  ";";
+                            $joined_club_result = $conn->query($joined_club_sql);
+                            $joined_club_result_check = mysqli_num_rows($joined_club_result);
+                            ?>
+
+                    <?php if ($joined_club_result_check > 0) : ?>
+                    <?php $joined_club_row = mysqli_fetch_assoc($joined_club_result) ?>
+                    <tr>
+                        <td class="padding-left"><?php echo $student_row['Student_name']; ?></td>
+                        <td class="padding-left"><?php echo $student_row['TP_number']; ?></td>
+                        <td class="padding-left"><?php echo $student_row['Gender']; ?></td>
+                        <td class="padding-left"><?php echo $student_row['Contact_number']; ?></td>
+                        <td class="padding-left"><?php echo $joined_club_row['Role'] ?></td>
+
+                        <td style="text-align: center;">
+                            <?php if ($joined_club_row['Role'] == "Member") : ?>
+                            <i data-modal-target="#view" title="View" class="fas fa-eye"
+                                id="view-button-<?php echo $student_ID; ?>"></i>
+                            <?php else : ?>
+                            <i data-modal-target="#edit" title="Edit" class="fas fa-edit"
+                                id="edit-button-<?php echo $student_ID; ?>"></i>
+                            <?php endif; ?>
+                            <a href="admin_students.php"><i title="Delete" class="fas fa-trash-alt"
+                                    id="delete-button-<?php echo $student_ID; ?>"></i>
+                            </a>
+
+                        </td>
+                    </tr>
+                    <?php endif; ?>
+                    <?php endwhile; ?>
+                    <?php else : ?>
+                    <tr>
+                        <td colspan="6">
+                            <h2 class="no-record">No Records Found</h2>
+                        </td>
+                    </tr>
+                    <?php endif; ?>
+                </table>
+            </div>
+        </div>
+    </article>
 </main>
+
 <script>
 // Get various elements by id
 var editButton = document.getElementById("edit-button");
@@ -164,8 +236,6 @@ window.onload = function() {
 <!-- Validate Email Exist Error Script -->
 <script defer src="scripts/admin_email_exist_error.js"></script>
 
-<!-- Validate TP Number Exist Error Script -->
-<script defer src="scripts/tp_exist_error.js"></script>
 
 <?php
 include_once "includes/footer.php";
--- admin/manage_specific_club.php
+++ admin/manage_specific_club.php
@@ -1,6 +1,31 @@
 <?php
 session_start();
 include_once "../tbc/includes/dbh.php";
+
+$id = $_SESSION['club_id'];
+if (isset($_POST['delete'])) {
+
+    $club_sql_query = "SELECT * FROM clubs WHERE Club_ID = $id;";
+    $club_result = mysqli_query($conn, $club_sql_query);
+    $row = mysqli_fetch_assoc($club_result);
+    $club_name = $row['Name'];
+
+    $delete_sql_query = "DELETE FROM clubs WHERE Club_ID = $id;";
+    $delete_result = mysqli_query($conn, $delete_sql_query);
+    if (mysqli_affected_rows($conn) >= 1) {
+        $_SESSION['message'] = "Record Deleted Successfully";
+        $_SESSION['delete'] = true;
+
+        unset($_SESSION['club_id']);
+        header("Location: admin_clubs.php");
+    } else {
+        $_SESSION['message'] = "Failed to Delete Record";
+        $_SESSION['delete'] = false;
+
+        header("Location: admin_specific_club.php?club=" . $club_name);
+    }
+}
+
 if (isset($_POST['update'])) {
 
     // Get data from HTML Form
@@ -12,8 +37,6 @@ if (isset($_POST['update'])) {
     $start_time = trim($_POST['start-time']);
     $end_time = trim($_POST['end-time']);
     $venue = trim($_POST['venue']);
-
-    $id = $_SESSION['club_id'];
 
     // Check if email exist or not
     $email_sql = "SELECT * FROM (
@@ -30,7 +53,7 @@ if (isset($_POST['update'])) {
     if ($result_check > 0) {
         $_SESSION['message'] = "Email already exists. Please enter another one.";
         $_SESSION['update'] = false;
-        header("Location: admin_specific_club.php?club=". $club_name);
+        header("Location: admin_specific_club.php?club=" . $club_name);
     } else {
         // if no file was uploaded to the club image field
         if (empty($_FILES['image']['tmp_name']) || !is_uploaded_file($_FILES['image']['tmp_name'])) {
@@ -56,8 +79,7 @@ if (isset($_POST['update'])) {
 
         unset($_SESSION['club_id']);
     }
-    
     mysqli_close($conn);
 
-    header("Location: admin_specific_club.php?club=". $club_name);
+    header("Location: admin_specific_club.php?club=" . $club_name);
 }
