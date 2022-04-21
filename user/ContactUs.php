9d752855b4e1a3fe8681a8abfb3075dca60f216d
JoeLJW <jooweelim@gmail.com>
2022-04-21 12:00:19 +0800

    Added about us and contact us

---
+++ user/ContactUs.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Club and Society Management System</title>
+    <!-- CSS Style -->
+    <link rel="stylesheet" href="css/ContactUs.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="container">
+    <div class="content">
+      <div class="left-side">
+        <div class="address details">
+          <i class="fas fa-map-marker-alt"></i>
+          <div class="topic">Location</div>
+          <div class="text-one">Technology Park</div>
+          <div class="text-two">Bukit Jalil</div>
+        </div>
+        <div class="phone details">
+          <i class="fas fa-phone-alt"></i>
+          <div class="topic">Phone</div>
+          <div class="text-one">+6012 345 6789</div>
+          <div class="text-two">+6019 876 5432</div>
+        </div>
+        <div class="email details">
+          <i class="fas fa-envelope"></i>
+          <div class="topic">Email</div>
+          <div class="text-one">blablabla@gmail.com</div>
+          <div class="text-two">blablabla@gmail.com</div>
+        </div>
+      </div>
+      <div class="right-side">
+        <div class="topic-text">Send us a message</div>
+        <p>If you have any problem in this website, you can send us message from here. It's our pleasure to help you.</p>
+      <form action="#">
+        <div class="input-box">
+          <input type="text" placeholder="Enter your name">
+        </div>
+        <div class="input-box">
+          <input type="text" placeholder="Enter your email">
+        </div>
+        <div class="input-box">
+          <input type="tel" placeholder="Enter your contact number">
+        </div>
+        <div class="input-box message-box">
+          <input type="text" placeholder="Enter your message">
+        </div>
+        <div class="button">
+          <input type="button" value="Send Now" >
+        </div>
+      </form>
+    </div>
+    </div>
+  </div>
+</body>
+</html>
