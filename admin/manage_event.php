a5513b13736cd3de163c2544f965d3ab3e23b598
Kohiro <liqi2002@gmail.com>
2022-04-24 12:02:40 +0800

    event approval

--- admin/manage_event.php
+++ admin/manage_event.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include_once "../user/includes/dbh.php";
+
 if (isset($_POST['update']) || (isset($_POST['add']))) {
 
     // Get data from HTML Form
@@ -11,10 +12,24 @@ if (isset($_POST['update']) || (isset($_POST['add']))) {
     $end_time = trim($_POST['end-time']);
     $approval_status = trim($_POST['approval-status']);
     $club_id = trim($_POST['organizing-club']);
+}
 
+
+if (isset($_POST['update']) || isset($_POST['approve']) || isset($_POST['reject'])) {
+    $id = $_SESSION['event_id'];
+
+    if (isset($_POST['approve'])) {
+        // Update approval status
+        $sql_query = "UPDATE events SET Approval_status = 'Approved' WHERE Event_ID = $id";
+    }
+
+
+    if (isset($_POST['reject'])) {
+        // Update approval status
+        $sql_query = "UPDATE events SET Approval_status = 'Rejected' WHERE Event_ID = $id";
+    }
 
     if (isset($_POST['update'])) {
-        $id = $_SESSION['event_id'];
         // if no file was uploaded to the event image field
         if (empty($_FILES['image']['tmp_name']) || !is_uploaded_file($_FILES['image']['tmp_name'])) {
             $sql_query = "UPDATE events SET Event_name = '$event_name', Description = '$event_description', Date = '$date', Start_time = '$start_time', End_time = '$end_time', Approval_status = '$approval_status', Club_ID = '$club_id'  WHERE Event_ID = $id";
@@ -23,44 +38,45 @@ if (isset($_POST['update']) || (isset($_POST['add']))) {
             $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
             $sql_query = "UPDATE events SET Event_name = '$event_name', Description = '$event_description', Date = '$date', Start_time = '$start_time', End_time = '$end_time', Approval_status = '$approval_status', Club_ID = '$club_id', Event_image = '$image' WHERE Event_ID = $id";
         }
-
-        $result = mysqli_query($conn, $sql_query);
-        // If database is updated
-        if (mysqli_affected_rows($conn) >= 1) {
-            $_SESSION['update'] = true;
-            $_SESSION['message'] = "Record Updated Successfully";
-        }
-
-        // If SQL fails to run
-        if ($result == false) {
-            $_SESSION['update'] = false;
-            $_SESSION['message'] = "Failed to Update Record";
-        }
-
-        unset($_SESSION['event_id']);
     }
 
-
-    if (isset($_POST['add'])) {
-        $date_posted = date("Y-m-d");
-
-        $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
-        $sql_query = "INSERT INTO events (Event_name, Description, Date, Start_time, End_time, Date_posted, Approval_status, Event_image, Admin_ID, Club_ID) VALUES ('$event_name', '$event_description', '$date', '$start_time', '$end_time', '$date_posted', 'Approved', '$image', 1, '$club_id');";
-        $result = mysqli_query($conn, $sql_query);
-
-        // If database is updated
-        if (mysqli_affected_rows($conn) >= 1) {
-            $_SESSION['add'] = true;
-            $_SESSION['message'] = "Record Added Successfully";
-        }
-
-        // If SQL fails to run
-        if ($result == false) {
-            $_SESSION['add'] = false;
-            $_SESSION['message'] = "Failed to Add Record";
-        }
+    $result = mysqli_query($conn, $sql_query);
+    // If database is updated
+    if (mysqli_affected_rows($conn) >= 1) {
+        $_SESSION['update'] = true;
+        $_SESSION['message'] = "Record Updated Successfully";
     }
-    mysqli_close($conn);
 
-    header("Location: admin_events.php");
+    // If SQL fails to run
+    if ($result == false) {
+        $_SESSION['update'] = false;
+        $_SESSION['message'] = "Failed to Update Record";
+    }
+
+    unset($_SESSION['event_id']);
 }
+
+if (isset($_POST['add'])) {
+    $date_posted = date("Y-m-d");
+
+    $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+    $sql_query = "INSERT INTO events (Event_name, Description, Date, Start_time, End_time, Date_posted, Approval_status, Event_image, Admin_ID, Club_ID) VALUES ('$event_name', '$event_description', '$date', '$start_time', '$end_time', '$date_posted', 'Approved', '$image', 1, '$club_id');";
+    $result = mysqli_query($conn, $sql_query);
+
+    // If database is updated
+    if (mysqli_affected_rows($conn) >= 1) {
+        $_SESSION['add'] = true;
+        $_SESSION['message'] = "Record Added Successfully";
+    }
+
+    // If SQL fails to run
+    if ($result == false) {
+        $_SESSION['add'] = false;
+        $_SESSION['message'] = "Failed to Add Record";
+    }
+}
+
+// Close Database Connection
+mysqli_close($conn);
+
+header("Location: admin_events.php");
