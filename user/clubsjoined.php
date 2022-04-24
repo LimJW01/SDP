e87a14e306b6ab0e1666d61983f5b0d59d0a4fa8
JoeLJW <jooweelim@gmail.com>
2022-04-24 18:09:52 +0800

    added clubs joined page (incomplete)

---
+++ user/clubsjoined.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+    .align-right {
+        text-align: right;
+        border: 0;
+        margin-right: 50px;
+    }
+
+    .title {
+        margin-left: 50px;
+    }
+    </style>
+
+    <title>Clubs Joined</title>
+</head>
+<body>
+<?php include('includes/navbar.php'); ?>
+<div class="title">
+        <h1>Clubs Joined<br></h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once "./includes/footer.php"; ?>
+    
+</body>
+</html>
