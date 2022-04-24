7a8cbd9ff328794813620795a8578dcc07a53da5
JoeLJW <jooweelim@gmail.com>
2022-04-24 18:09:29 +0800

    added specific event page

--- user/event.php
+++ user/event.php
@@ -40,7 +40,7 @@
         <h1>Events<br></h1>
     </div>
     <div class="section1">
-        <a onclick="location.href='club.php';">
+        <a onclick="location.href='specificevent.php';">
             <figure class="figure">
                 <img src="image/taekwondo.png" width="400" height="350">
                 <figcaption class="figure-caption">A caption for the above image.</figcaption>
