415c251ec5cc375dd4da25509b166b196667b9e7
JoeLJW <jooweelim@gmail.com>
2022-04-25 19:11:14 +0800

    container displaying in row (incomplete)

--- user/index.php
+++ user/index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/product.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -44,7 +44,7 @@
         <?php } else { ?>
         <?php } ?>
     </div>
-    <div class="card column" style='width: 190px; margin-left: 50px;'>
+    <div class="card1 column1">
         <?php $sql = "SELECT * FROM clubs ORDER BY Club_name ASC";
                 $result = $conn->query($sql);
                 $result_check = mysqli_num_rows($result);
@@ -55,6 +55,7 @@
                 <div class='grid-item'>
                     <div class="img-container">
                         <img title="<?php echo $row['Club_name']; ?>"
+                            height="175" width="250"                        
                             src="data:image/jpeg;base64,<?php echo base64_encode($row['Club_image']); ?>"
                             alt='club_image'>
                     </div>
