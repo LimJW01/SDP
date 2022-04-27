e02d4dca6d1bbb4c723fd9dfb461517edec7f190
JoeLJW <jooweelim@gmail.com>
2022-04-27 10:17:06 +0800

    add all extra info for comittee in specific club

---
+++ user/com_club_details.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Specific Club</title>
+</head>
+<body>
+    <?php include_once "includes/navbar.php";?>
+    <div class="reqtitle">
+        <h1>Club Details</h1>
+    </div>
+    <div class="table-users">
+    <div class="header">Members</div>
+        <table cellspacing="0">
+        <tr>
+            <th style="text-align:center" width="130">Student Name</th>
+            <th style="text-align:center" width="110">TP Number</th>
+            <th style="text-align:center" width="80">Gender</th>
+            <th style="text-align:center" width="190">Email</th>
+            <th style="text-align:center" width="130">Contact</th>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+        <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+        <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+        <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        </table>
+    </div>
+    <div class="table-users">
+    <div class="header">Events</div>
+        <table cellspacing="0">
+        <tr>
+            <th style="text-align:center" width="130">Event Name</th>
+            <th style="text-align:center" width="110">Description</th>
+            <th style="text-align:center" width="80">Date</th>
+            <th style="text-align:center" width="190">Start Time</th>
+            <th style="text-align:center" width="130">End Time</th>
+            <th style="text-align:center" width="190">Date Posted</th>
+            <th style="text-align:center" width="130">Approval Status</th>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        </table>
+    </div>
+    <div class="table-users">
+    <div class="header">Activities</div>
+        <table cellspacing="0">
+        <tr>
+            <th style="text-align:center" width="400">Description</th>
+            <th style="text-align:center" width="80">Day</th>
+            <th style="text-align:center" width="80">Start Time</th>
+            <th style="text-align:center" width="80">End Time</th>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>4</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        </table>
+    </div>
+
+    <?php include_once "includes/footer.php";?>
+</body>
+</html>
