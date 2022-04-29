8ac9920143b17c8e11089d972258118e00835781
YeeSiang708 <103414657+YeeSiang708@users.noreply.github.com>
2022-04-29 15:51:34 +0800

    init

--- user/about_us.php
+++ user/about_us.php
@@ -10,6 +10,8 @@ include_once "includes/header.php";
         <div class="content">
             <h2>Our University</h2>
             <p>
+                Asia Pacific University of Technology & Innovation (APU) is amongst Malaysia’s Premier Private Universities, and is where a unique fusion of technology, innovation and creativity works effectively towards transforming students into highly competent, employable and future-proof professionals. 
+                APU has earned an enviable reputation as an award-winning University through its achievements in winning a host of over 400 prestigious awards at local and international levels.
                 We provide high quality, affordable, innovative and internationally benchmarked education and
                 research in a professional,
                 ethical and student centred manner by designing and delivering a range of enriching and distinctive
@@ -25,8 +27,10 @@ include_once "includes/header.php";
         <div class="content">
             <h2>Our Clubs and Societies</h2>
             <p>
-                Societes & Special Interest Groups allow forums for staff and students to communicate ideas and best
-                practices to support staff development and student learning.
+            Being a university student, it is also important to take time for sports & recreation activities to keep you excelling because university isn’t just about studies. 
+            Take a break, we have our social & recreation space for you to relax with your best buddies during your break. 
+            Not just that, there’s an array of events, activities, trips, & roadshows, are taking place accross the year.
+            Societies & Special Interest Groups (SIG’s) allow forums for staff and students to communicate ideas and best practices to support staff development and student learning.
             </p>
         </div>
     </section>
