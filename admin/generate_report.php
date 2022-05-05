ef3a5e05234db4330e633b566c589b8427272f26
Kohiro <liqi2002@gmail.com>
2022-05-05 14:38:47 +0800

    init

--- admin/generate_report.php
+++ admin/generate_report.php
@@ -1,14 +1,76 @@
 <?php
-
 include_once "../user/includes/dbh.php";
 session_start();
+
 $report_category = $_POST['report_category'];
-$report_list = $_POST['report_list'];
+$subject_id = $_POST['report_list'];
 
 $data_array = array();
+?>
 
-if ($report_category == "Students") {
-    if ($report_list == "all-student") {
+<!-- Student Report Data -->
+<?php if ($report_category == "Students") : ?>
+
+
+
+<?php if ($subject_id == "all-student") : ?>
+<!-- All Student Table  -->
+<div class="table-container" id="student-report">
+    <table>
+        <tr>
+            <th class="padding-left">Student Name</th>
+            <th class="padding-left">TP Number</th>
+            <th class="padding-left">Gender</th>
+            <th class="padding-left">Clubs Joined</th>
+            <th class="padding-left">Contact Number</th>
+        </tr>
+        <?php
+                $student_sql = "SELECT * FROM students ORDER BY Student_name ASC";
+                $student_result = $conn->query($student_sql);
+                $student_result_check = mysqli_num_rows($student_result);
+                $student_array = array();
+                ?>
+        <?php if ($student_result_check > 0) : ?>
+        <?php while ($student_row = mysqli_fetch_assoc($student_result)) : ?>
+        <?php
+                        $joined_club_sql = "SELECT * FROM joined_clubs WHERE Student_ID = " . $student_row['Student_ID'] .  ";";
+                        $joined_club_result = $conn->query($joined_club_sql);
+                        $joined_club_result_check = mysqli_num_rows($joined_club_result);
+                        ?>
+
+        <?php $joined_clubs = "No Clubs Joined" ?>
+        <?php if ($joined_club_result_check > 0) : ?>
+        <?php $joined_clubs_array = array() ?>
+        <?php while ($joined_club_row = mysqli_fetch_assoc($joined_club_result)) : ?>
+        <?php
+                                $joined_club_id =  $joined_club_row['Club_ID'];
+                                $club_sql = "SELECT * FROM Clubs WHERE Club_ID = $joined_club_id;";
+                                $club_result = $conn->query($club_sql);
+                                $club_row = mysqli_fetch_assoc($club_result);
+                                array_push($joined_clubs_array, $club_row['Club_name']);
+                                ?>
+        <?php endwhile; ?>
+        <?php $joined_clubs = join(", ", $joined_clubs_array) ?>
+        <?php endif; ?>
+        <tr>
+            <td class="padding-left"><?php echo $student_row['Student_name']; ?></td>
+            <td class="padding-left"><?php echo $student_row['TP_number']; ?></td>
+            <td class="padding-left"><?php echo $student_row['Gender']; ?></td>
+            <td class="padding-left"><?php echo $joined_clubs ?></td>
+            <td class="padding-left"><?php echo $student_row['Contact_number']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+        <?php else : ?>
+        <tr>
+            <td colspan="5">
+                <h2 class="no-record">No Records Found</h2>
+            </td>
+        </tr>
+        <?php endif; ?>
+    </table>
+</div>
+
+<?php
         $header = "Student Club Distribution";
 
         array_push($data_array, ["Club Name", "Total Members"]);
@@ -33,20 +95,111 @@ if ($report_category == "Students") {
                 }
             }
         }
-    }
-}
+        ?>
+<?php else : ?>
 
+<?php endif; ?>
+<?php endif; ?>
 
-if ($report_category == "Clubs") {
-}
+<?php if ($report_category == "Clubs") : ?>
 
+<div class="table-container">
+    <table>
+        <tr>
+            <th class="padding-left">Club Name</th>
+            <th class="padding-left">Email</th>
+            <th class="padding-left">Contact Number</th>
+        </tr>
+        <?php
+            $club_sql = "SELECT * FROM clubs";
+            $club_result = $conn->query($club_sql);
+            $club_result_check = mysqli_num_rows($club_result);
+            ?>
+        <?php if ($club_result_check > 0) : ?>
+        <?php while ($club_row = mysqli_fetch_assoc($club_result)) : ?>
 
-if ($report_category == "Events") {
-}
-?>
-<div class="table container">
-
+        <tr>
+            <td class="padding-left"><?php echo $club_row['Club_name']; ?></td>
+            <td class="padding-left"><?php echo $club_row['Email']; ?></td>
+            <td class="padding-left"><?php echo $club_row['Contact_number']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+        <?php else : ?>
+        <tr>
+            <td colspan="3">
+                <h2 class="no-record">No Records Found</h2>
+            </td>
+        </tr>
+        <?php endif; ?>
+    </table>
 </div>
+
+<?php endif; ?>
+
+<?php if ($report_category == "Events") : ?>
+
+<div class="table-container">
+    <table>
+        <tr>
+            <th class="padding-left">Event Name</th>
+            <th class="padding-left">Date</th>
+            <th class="padding-left">Start Time</th>
+            <th class="padding-left">End Time</th>
+        </tr>
+        <?php
+            $event_sql = "SELECT * FROM events";
+            $event_result = $conn->query($event_sql);
+            $event_result_check = mysqli_num_rows($event_result);
+            ?>
+        <?php if ($event_result_check > 0) : ?>
+        <?php while ($event_row = mysqli_fetch_assoc($event_result)) : ?>
+
+        <tr>
+            <td class="padding-left"><?php echo $event_row['Event_name']; ?></td>
+            <td class="padding-left"><?php echo $event_row['Date']; ?></td>
+            <td class="padding-left"><?php echo change_time_format($event_row['Start_time']); ?></td>
+            <td class="padding-left"><?php echo change_time_format($event_row['End_time']); ?></td>
+        </tr>
+        <?php endwhile; ?>
+        <?php else : ?>
+        <tr>
+            <td colspan="4">
+                <h2 class="no-record">No Records Found</h2>
+            </td>
+        </tr>
+        <?php endif; ?>
+    </table>
+</div>
+
+<?php
+    // pie chart
+    $header = "Club Event Distribution";
+
+    array_push($data_array, ["Club Name", "Total Events"]);
+
+    $club_sql = "SELECT * FROM clubs";
+    $club_result = $conn->query($club_sql);
+    $club_result_check = mysqli_num_rows($club_result);
+
+    if ($club_result_check > 0) {
+        while ($club_row = mysqli_fetch_assoc($club_result)) {
+            $club_id = $club_row['Club_ID'];
+            $club_name = $club_row['Club_name'];
+            $event_sql = "SELECT COUNT(Event_ID) AS total_events FROM events WHERE Club_ID = '$club_id'; ";
+            $event_sql_result = $conn->query($event_sql);
+            $event_row = mysqli_fetch_assoc($event_sql_result);
+            $total_events = (int) $event_row['total_events'];
+
+
+            if ($total_events != 0) {
+                $event_data = [$club_name, $total_events];
+                array_push($data_array, $event_data);
+            }
+        }
+    }
+    ?>
+<?php endif; ?>
+
 
 <!-- Pie Chart -->
 <div id="piechart" style="width: 900px; height: 500px;"></div>
@@ -54,7 +207,7 @@ if ($report_category == "Events") {
 
 
 <!-- Load Pie Chart Data -->
-<?php if ($report_list == "all-student" || $report_list == "all-event") : ?>
+<?php if ($subject_id == "all-students" || $subject_id == "all-events") : ?>
 <script type="text/javascript">
 google.charts.load('current', {
     'packages': ['corechart']
