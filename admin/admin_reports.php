d854a21a397995cbe5466bd30ad9dfd701e4c082
Kohiro <liqi2002@gmail.com>
2022-05-05 13:42:31 +0800

    report

--- admin/admin_reports.php
+++ admin/admin_reports.php
@@ -7,7 +7,7 @@ include_once "includes/header.php";
     <hr>
     <article id="reports">
         <div class="content-container">
-            <form action="generate_report.php" method="post" onsubmit="return validate_report();">
+            <form method="post" onsubmit="return validate_report();">
                 <div class="report-container">
                     <ul class="flex-container">
                         <li class="flex-item">
@@ -39,107 +39,36 @@ include_once "includes/header.php";
                         </li>
                     </ul>
                 </div>
+
             </form>
+            <div id="report-container">
 
-            <div class="table-container" id="report-table">
-                <table>
-                    <tr>
-                        <th class="padding-left">Student Name</th>
-                        <th class="padding-left">TP Number</th>
-                        <th class="padding-left">Gender</th>
-                        <th class="padding-left">Clubs Joined</th>
-                        <th class="padding-left">Contact Number</th>
-                        <th style="text-align: center;">Actions</th>
-                    </tr>
-                    <?php
-                    if (isset($_POST['search']) && !empty(trim($_POST['search-field']))) {
-                        $search = trim($_POST['search-field']);
-                        $student_sql = "SELECT * FROM students WHERE Student_name LIKE '%$search%' ORDER BY Student_name ASC";
-                    } else {
-                        $student_sql = "SELECT * FROM students ORDER BY Student_name ASC";
-                    }
-
-                    $student_result = $conn->query($student_sql);
-                    $student_result_check = mysqli_num_rows($student_result);
-                    $student_array = array();
-                    ?>
-                    <?php if ($student_result_check > 0) : ?>
-                    <?php while ($student_row = mysqli_fetch_assoc($student_result)) : ?>
-                    <?php
-                            $student_ID = "S" . $student_row['Student_ID'];
-                            array_push($student_array, $student_ID);
-
-                            $joined_club_sql = "SELECT * FROM joined_clubs WHERE Student_ID = " . $student_row['Student_ID'] .  ";";
-                            $joined_club_result = $conn->query($joined_club_sql);
-                            $joined_club_result_check = mysqli_num_rows($joined_club_result);
-                            ?>
-
-                    <?php $joined_clubs = "No Clubs Joined" ?>
-                    <?php if ($joined_club_result_check > 0) : ?>
-                    <?php $joined_clubs_array = array() ?>
-                    <?php while ($joined_club_row = mysqli_fetch_assoc($joined_club_result)) : ?>
-                    <?php
-                                    $joined_club_id =  $joined_club_row['Club_ID'];
-                                    $club_sql = "SELECT * FROM Clubs WHERE Club_ID = $joined_club_id;";
-                                    $club_result = $conn->query($club_sql);
-                                    $club_row = mysqli_fetch_assoc($club_result);
-                                    array_push($joined_clubs_array, $club_row['Club_name']);
-                                    ?>
-                    <?php endwhile; ?>
-                    <?php $joined_clubs = join(", ", $joined_clubs_array) ?>
-                    <?php endif; ?>
-                    <tr>
-                        <td class="padding-left"><?php echo $student_row['Student_name']; ?></td>
-                        <td class="padding-left"><?php echo $student_row['TP_number']; ?></td>
-                        <td class="padding-left"><?php echo $student_row['Gender']; ?></td>
-                        <td class="padding-left"><?php echo $joined_clubs ?></td>
-                        <td class="padding-left"><?php echo $student_row['Contact_number']; ?></td>
-                        <td style="text-align: center;">
-                            <i data-modal-target="#view" title="View" class="fas fa-eye"
-                                id="view-button-<?php echo $student_ID; ?>"></i>
-                            <i data-modal-target="#edit" title="Edit" class="fas fa-edit"
-                                id="edit-button-<?php echo $student_ID; ?>"></i>
-                            <a href="admin_students.php"><i title="Delete" class="fas fa-trash-alt"
-                                    id="delete-button-<?php echo $student_ID; ?>"></i>
-                            </a>
-                        </td>
-                    </tr>
-                    <?php endwhile; ?>
-                    <?php else : ?>
-                    <tr>
-                        <td colspan="7">
-                            <h2 class="no-record">No Records Found</h2>
-                        </td>
-                    </tr>
-                    <?php endif; ?>
-                </table>
             </div>
         </div>
     </article>
 </main>
 
-<script src="scripts/add_report_selection.js"></script>
+<script defer src="scripts/add_report_selection.js"></script>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-google.charts.load("current", {
-    packages: ["corechart"]
+<script defer type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<!-- When Generate button clicked -->
+<?php if (isset($_POST['generate'])) : ?>
+<?php
+    $report_category = trim($_POST['report-category']);
+    $report_list = trim($_POST['report-list']);
+    ?>
+<script>
+$(document).ready(function() {
+    var report_category = "<?php echo $report_category ?>";
+    var report_list = "<?php echo $report_list ?>";
+    $("#report-container").load("generate_report.php", {
+        report_category: report_category,
+        report_list: report_list
+    });
 });
-// google.charts.setOnLoadCallback(drawChart);
-
-function drawChart(header, data_array) {
-    var data = google.visualization.arrayToDataTable(data_array);
-
-    var options = {
-        title: header,
-        is3D: true,
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-    chart.draw(data, options);
-}
 </script>
-
+<?php endif; ?>
 <?php
 include_once "includes/alert_message.php";
 include_once "includes/footer.php";
--- admin/generate_report.php
+++ admin/generate_report.php
@@ -1,1 +1,82 @@
+<?php
+
+include_once "../user/includes/dbh.php";
+session_start();
+$report_category = $_POST['report_category'];
+$report_list = $_POST['report_list'];
+
+$data_array = array();
+
+if ($report_category == "Students") {
+    if ($report_list == "all-student") {
+        $header = "Student Club Distribution";
+
+        array_push($data_array, ["Club Name", "Total Members"]);
+
+        $club_sql = "SELECT * FROM clubs";
+        $club_result = $conn->query($club_sql);
+        $club_result_check = mysqli_num_rows($club_result);
+
+        if ($club_result_check > 0) {
+            while ($club_row = mysqli_fetch_assoc($club_result)) {
+                $club_id = $club_row['Club_ID'];
+                $club_name = $club_row['Club_name'];
+                $joined_club_sql = "SELECT COUNT(Student_ID) AS total_members FROM joined_clubs WHERE Club_ID = '$club_id'; ";
+                $joined_club_result = $conn->query($joined_club_sql);
+                $joined_club_row = mysqli_fetch_assoc($joined_club_result);
+                $total_member = (int) $joined_club_row['total_members'];
+
+
+                if ($total_member != 0) {
+                    $student_data = [$club_name, $total_member];
+                    array_push($data_array, $student_data);
+                }
+            }
+        }
+    }
+}
+
+
+if ($report_category == "Clubs") {
+}
+
+
+if ($report_category == "Events") {
+}
+?>
+<div class="table container">
+
+</div>
+
+<!-- Pie Chart -->
 <div id="piechart" style="width: 900px; height: 500px;"></div>
+
+
+
+<!-- Load Pie Chart Data -->
+<?php if ($report_list == "all-student" || $report_list == "all-event") : ?>
+<script type="text/javascript">
+google.charts.load('current', {
+    'packages': ['corechart']
+});
+
+google.charts.setOnLoadCallback(function() {
+    drawChart("<?php echo $header; ?>", <?php echo json_encode($data_array); ?>);
+});
+
+
+function drawChart(header, data_array) {
+
+    var data = google.visualization.arrayToDataTable(data_array);
+
+    var options = {
+        title: header,
+        is3D: true
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
+</script>
+<?php endif; ?>
